94b8163e23d972e50e889f90478c0552f60ad176
suld2495 <suld7777@gmail.com>
2024-06-08 15:11:33 +0900

    feat: 유저 테이블 생성

--- src/main/java/com/tour/prevel/auth/domain/User.java
+++ src/main/java/com/tour/prevel/auth/domain/User.java
@@ -3,20 +3,25 @@ package com.tour.prevel.auth.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
-@Table
+@Table(name = "TB_USER")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
     private String email;
+
     private String password;
+
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return new ArrayList<>();
