f3b37f1312eecdffeb8a0c823e944cb3d6e94e64
suld2495 <suld7777@gmail.com>
2024-09-13 18:44:30 +0900

    로그 추가

--- src/main/java/com/tour/prevel/config/PublicKeyConverter.java
+++ src/main/java/com/tour/prevel/config/PublicKeyConverter.java
@@ -1,5 +1,6 @@
 package com.tour.prevel.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.converter.RsaKeyConverters;
@@ -8,12 +9,14 @@ import org.springframework.stereotype.Component;
 import java.io.ByteArrayInputStream;
 import java.security.interfaces.RSAPublicKey;
 
+@Slf4j
 @Component
 @ConfigurationPropertiesBinding
 public class PublicKeyConverter implements Converter<String, RSAPublicKey> {
 
     @Override
     public RSAPublicKey convert(String from) {
+        log.info("Converting public key", from);
         return RsaKeyConverters.x509().convert(new ByteArrayInputStream(from.getBytes()));
     }
 }
