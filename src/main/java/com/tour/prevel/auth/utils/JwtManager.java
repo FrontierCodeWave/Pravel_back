4f6409f38643446507d7ba5eae112f2cb296b5d5
suld2495 <suld7777@gmail.com>
2024-09-13 21:21:38 +0900

    feat: 키 정제

--- src/main/java/com/tour/prevel/auth/utils/JwtManager.java
+++ src/main/java/com/tour/prevel/auth/utils/JwtManager.java
@@ -6,6 +6,8 @@ import org.springframework.security.converter.RsaKeyConverters;
 import java.io.ByteArrayInputStream;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @Getter
 @ConfigurationProperties(prefix = "auth.key")
@@ -14,8 +16,19 @@ public class JwtManager {
     private RSAPublicKey publicKey;
     private RSAPrivateKey privateKey;
 
+    public static byte[] replaceBytes(byte[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 92) {
+                input[i] = 10;
+            }
+        }
+        return input;
+    }
+
     public JwtManager(String publicKey, String privateKey) {
-        this.publicKey = RsaKeyConverters.x509().convert(new ByteArrayInputStream(publicKey.getBytes()));;
-        this.privateKey = RsaKeyConverters.pkcs8().convert(new ByteArrayInputStream(privateKey.getBytes()));;
+        this.publicKey = RsaKeyConverters.x509().convert(new ByteArrayInputStream(
+                replaceBytes(publicKey.replace("\\", System.lineSeparator()).getBytes())));
+        this.privateKey = RsaKeyConverters.pkcs8().convert(new ByteArrayInputStream(
+                replaceBytes(privateKey.replace("\\", System.lineSeparator()).getBytes())));
     }
 }
