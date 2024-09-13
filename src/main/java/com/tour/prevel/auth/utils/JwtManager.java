de363cd77d1e32ba55865d244728c07657464141
suld2495 <suld7777@gmail.com>
2024-09-13 18:53:31 +0900

    fix: 키 직접 컨버팅

--- src/main/java/com/tour/prevel/auth/utils/JwtManager.java
+++ src/main/java/com/tour/prevel/auth/utils/JwtManager.java
@@ -1,17 +1,21 @@
 package com.tour.prevel.auth.utils;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.converter.RsaKeyConverters;
 
+import java.io.ByteArrayInputStream;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 @Getter
 @ConfigurationProperties(prefix = "auth.key")
-@AllArgsConstructor
 public class JwtManager {
 
     private RSAPublicKey publicKey;
     private RSAPrivateKey privateKey;
+
+    public JwtManager(String publicKey, String privateKey) {
+        this.publicKey = RsaKeyConverters.x509().convert(new ByteArrayInputStream(publicKey.getBytes()));;
+        this.privateKey = RsaKeyConverters.pkcs8().convert(new ByteArrayInputStream(privateKey.getBytes()));;
+    }
 }
